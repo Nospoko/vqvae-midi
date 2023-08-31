e7d58b0e20cf9449dfd5468c325d4536c1f7dc84
Samuel Janas <sneakyinsect@gmail.com>
2023-08-31 11:24:23 +0200

    added wandb

--- train.py
+++ train.py
@@ -1,12 +1,11 @@
 import hydra
 import torch
-import wandb
 import numpy as np
 from tqdm import tqdm
 from torch import nn, optim
-import matplotlib.pyplot as plt
 from omegaconf import OmegaConf, DictConfig
 
+import wandb
 from model.VQVAE import VQVAE
 from utils.data_loader import create_loaders
 
@@ -16,39 +15,47 @@ def count_parameters(model):
     print(f"The model has {sum(p.numel() for p in model.parameters() if p.requires_grad):,} trainable parameters.")
 
 
-def show_midi_random_pitch(start, duration, velocity, start_recon, duration_recon, velocity_recon, title):
-    # reapeat 60, 62, 64, 65, 67 for pitch
-    pitch = np.repeat(np.array([60, 62, 64, 65, 67]), 1024 // 5)
+def set_seed(seed):
+    # https://pytorch.org/docs/stable/notes/randomness.html
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
 
-    # print value ranges:
-    print("Reconstructed ranges: ")
-    print(f"start: {np.min(start_recon)} - {np.max(start_recon)}")
-    print(f"duration: {np.min(duration_recon)} - {np.max(duration_recon)}")
-    print(f"velocity: {np.min(velocity_recon)} - {np.max(velocity_recon)}")
 
-    print("Original ranges: ")
-    print(f"start: {np.min(start)} - {np.max(start)}")
-    print(f"duration: {np.min(duration)} - {np.max(duration)}")
-    print(f"velocity: {np.min(velocity)} - {np.max(velocity)}")
+def test(model, test_loader, criterion, cfg, epoch):
+    model.eval()
+    total_test_loss = 0
+    total_reconstruction_loss = 0
+    n_test = 0
 
-    # 2 charts next to each other
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 5))
-    for s, e, v, p in zip(start, duration, velocity, pitch):
-        ax1.broken_barh([(s, e - s)], (p, 1), facecolors=(0.0, v / 127.0, 0.0))
-        ax1.text(s + 0.5 * (e - s), p + 0.5, str(v), horizontalalignment="center", verticalalignment="center")
+    with torch.no_grad():
+        for batch in test_loader:
+            x_combined = torch.stack([batch["start"], batch["duration"], batch["velocity"]], dim=1)
 
-    for s, e, v, p in zip(start_recon, duration_recon, velocity_recon, pitch):
-        ax2.broken_barh([(s, e - s)], (p, 1), facecolors=(0.0, v / 127.0, 0.0))
-        ax2.text(s + 0.5 * (e - s), p + 0.5, str(v), horizontalalignment="center", verticalalignment="center")
+            if cfg.system.cuda:
+                x_combined = x_combined.to("cuda")
 
-    ax1.grid(True)
-    ax1.set_title("Original")
-    ax2.grid(True)
-    ax2.set_title("Reconstructed")
+            reconstructed_x, vq_loss, losses, perplexity, _, _ = model(x_combined)
 
-    plt.suptitle(title)
-    # save the figure
-    plt.savefig(f"results/{title}.png")
+            # Compute the loss
+            reconstruction_loss = criterion(reconstructed_x, x_combined)
+            total_reconstruction_loss += reconstruction_loss.item()
+
+            loss = reconstruction_loss + vq_loss
+            total_test_loss += loss.item()
+            n_test += 1
+
+        avg_test_loss = total_test_loss / n_test
+        avg_reconstruction_loss = total_reconstruction_loss / n_test
+
+        # Log the test metrics to wandb
+        wandb.log(
+            {
+                "test/Average Test Loss": avg_test_loss,
+                "test/Reconstruction Loss": avg_reconstruction_loss,
+            }
+        )
+        print(f"Epoch {epoch}, Average Test Loss: {avg_test_loss}")
 
 
 def train(model, train_loader, optimizer, criterion, cfg, epoch):
@@ -64,8 +71,7 @@ def train(model, train_loader, optimizer, criterion, cfg, epoch):
         optimizer.zero_grad()
         x_combined = torch.stack([batch["start"], batch["duration"], batch["velocity"]], dim=1)
 
-        if cfg.system.cuda:
-            x_combined = x_combined.to("cuda")
+        x_combined = x_combined.to(cfg.system.device)
 
         reconstructed_x, vq_loss, losses, perplexity, _, _ = model(x_combined)
 
@@ -82,16 +88,17 @@ def train(model, train_loader, optimizer, criterion, cfg, epoch):
         optimizer.step()
         n_train += 1
 
+        if batch_idx + 1 % cfg.train.log_interval == 0:
+            wandb.log(
+                {
+                    "train/Total Train Loss": total_train_loss / n_train,
+                    "train/Reconstruction Loss": total_recon_error / n_train,
+                    "train/VQ Loss": vq_loss.item(),
+                    "train/Perplexity": perplexity,
+                }
+            )
         # Update tqdm loop
         progress_bar.set_description(f"Epoch {epoch} Loss: {losses['loss']:.4f}")
-
-        # if (batch_idx + 2) % cfg.train.log_interval == 0:
-        #     perplexity_value = perplexity.item()
-        #     print(f"Epoch {epoch}: loss {losses['loss']:.4f} perplexity {perplexity_value:.3f}")
-        #     velocity = x_combined[0, 2].detach().cpu().numpy()
-        #     velocity_recon = reconstructed_x[0, 2].detach().cpu().numpy()
-        #     print(f"Velocity: {np.min(velocity)} - {np.max(velocity)}, shape: {velocity.shape}")
-        #     print(f"Velocity recon: {np.min(velocity_recon)} - {np.max(velocity_recon)}, shape: {velocity_recon.shape}")
 
     # Save checkpoint
     checkpoint = {
@@ -104,19 +111,16 @@ def train(model, train_loader, optimizer, criterion, cfg, epoch):
 
 @hydra.main(version_base=None, config_path="configs", config_name="config")
 def main(cfg: DictConfig):
-    if cfg.logger.enable_wandb:
-        name = f"{cfg.dataset.name}_{cfg.run_date}"
-        wandb.init(
-            project="MIDI VQ-VAE",
-            name=name,
-            config=OmegaConf.to_container(cfg, resolve=True),
-        )
+    name = f"MIDI_VQ-VAE_{cfg.run_date}"
+    wandb.init(
+        project="MIDI VQ-VAE",
+        name=name,
+        config=OmegaConf.to_container(cfg, resolve=True),
+    )
 
-    device = "cuda" if cfg.system.cuda else "cpu"
-
-    model = VQVAE(cfg.model, device)
-    model.to(device)
-    count_parameters(model)
+    model = VQVAE(cfg.model, cfg.system.device)
+    model.to(cfg.system.device)
+    # count_parameters(model)
 
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=cfg.train.lr)
@@ -126,6 +130,7 @@ def main(cfg: DictConfig):
     # Train the model
     for epoch in range(1, cfg.train.epochs + 1):
         train(model, train_loader, optimizer, criterion, cfg, epoch)
+        test(model, train_loader, criterion, cfg, epoch)
 
 
 if __name__ == "__main__":
