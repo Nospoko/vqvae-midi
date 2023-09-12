b3904ddd0b8baf2e66e63f22cc96d9458a7f32ab
Samuel Janas <sneakyinsect@gmail.com>
2023-09-12 09:46:04 +0200

    move eval to evals

--- evals/show_ecg.py
+++ evals/show_ecg.py
@@ -6,13 +6,13 @@ from evals.checkpoint_tools import load_checkpoint
 from utils.visualizations import draw_ecg_reconstructions
 
 if __name__ == "__main__":
-    ckpt_path = "checkpoints/2023_09_11_11_29_ECG_4.pt"
+    ckpt_path = "checkpoints/64embeddings_100dim_128hidden.pt"
     checkpoint, cfg, model = load_checkpoint(ckpt_path)
 
     _, validation_loader, _ = create_ecg_loaders(cfg, seed=cfg.system.seed)
 
     model.eval()
-    n_samples = 16
+    n_samples = 10
     idxs = np.random.randint(len(validation_loader.dataset), size=n_samples)
     signals = validation_loader.dataset[idxs]["signal"]
     draw_ecg_reconstructions(model, signals)
